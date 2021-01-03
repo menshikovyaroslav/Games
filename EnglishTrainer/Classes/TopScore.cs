d4ece0a2dd05557eb5de07f46d6b7ff7487427a1
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2021-01-03 18:02:02 +0700

    Класс TopScore для чтения/сохранения рекордов

---
+++ EnglishTrainer/Classes/TopScore.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishTrainer.Classes
+{
+    public static class TopScore
+    {
+        public static void SetTopScore(TopScoreResult scoreResult)
+        {
+            try
+            {
+                var hkcu = Registry.CurrentUser;
+                RegistryKey scoresBranch = hkcu.CreateSubKey("Software\\EnglishTrainer");
+                switch (scoreResult.Level)
+                {
+                    case Level.Beginner:
+                        var l1Branch = scoresBranch.CreateSubKey("Beginner");
+                        l1Branch.SetValue(scoreResult.Name, scoreResult.ToSaveFormat());
+                        l1Branch.Close();
+                        break;
+                    case Level.Standard:
+                        var l2Branch = scoresBranch.CreateSubKey("Standard");
+                        l2Branch.SetValue(scoreResult.Name, scoreResult.ToSaveFormat());
+                        l2Branch.Close();
+                        break;
+                    case Level.Advanced:
+                        var l3Branch = scoresBranch.CreateSubKey("Advanced");
+                        l3Branch.SetValue(scoreResult.Name, scoreResult.ToSaveFormat());
+                        l3Branch.Close();
+                        break;
+                }
+
+                scoresBranch?.Close();
+                hkcu?.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+
+        }
+
+        public static List<TopScoreResult> GetTopScores()
+        {
+            List<TopScoreResult> result = new List<TopScoreResult>();
+
+            try
+            {
+                var hkcu = Registry.CurrentUser;
+                RegistryKey scoresBranch = hkcu.CreateSubKey("Software\\EnglishTrainer");
+
+                var l1Branch = scoresBranch.CreateSubKey("Beginner");
+                var l2Branch = scoresBranch.CreateSubKey("Standard");
+                var l3Branch = scoresBranch.CreateSubKey("Advanced");
+
+                var l1Keys = l1Branch.GetValueNames();
+                foreach (var l1Key in l1Keys)
+                {
+                    var l1Value = ((string[])l1Branch.GetValue(l1Key));
+                    var topScoreResult = new TopScoreResult(l1Value);
+                    result.Add(topScoreResult);
+                }
+                l1Branch.Close();
+
+                var l2Keys = l2Branch.GetValueNames();
+                foreach (var l2Key in l2Keys)
+                {
+                    var l2Value = ((string[])l2Branch.GetValue(l2Key));
+                    var topScoreResult = new TopScoreResult(l2Value);
+                    result.Add(topScoreResult);
+                }
+                l2Branch.Close();
+
+                var l3Keys = l3Branch.GetValueNames();
+                foreach (var l3Key in l3Keys)
+                {
+                    var l3Value = ((string[])l3Branch.GetValue(l3Key));
+                    var topScoreResult = new TopScoreResult(l3Value);
+                    result.Add(topScoreResult);
+                }
+                l3Branch.Close();
+
+                scoresBranch?.Close();
+                hkcu?.Close();
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return result;
+        }
+    }
+}
---
+++ EnglishTrainer/Classes/TopScoreResult.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishTrainer.Classes
+{
+    public class TopScoreResult
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public Level Level { get; set; }
+
+        public TopScoreResult(string name, int score, Level level)
+        {
+            Name = name;
+            Score = score;
+            Level = level;
+        }
+
+        public string[] ToSaveFormat()
+        {
+            var result = new string[3];
+            result[0] = Name;
+            result[1] = Score.ToString();
+            result[2] = Level.ToString();
+
+            return result;
+        }
+
+        public TopScoreResult(string[] topScoreResult)
+        {
+            if (topScoreResult != null && topScoreResult.Length == 3)
+            {
+                Name = topScoreResult[0];
+                var score = 0;
+                Int32.TryParse(topScoreResult[1], out score);
+                Score = score;
+                var level = 0;
+                Int32.TryParse(topScoreResult[2], out level);
+                Level = (Level)level;
+            }
+        }
+    }
+
+    public enum Level
+    {
+        Beginner = 1,
+        Standard = 2,
+        Advanced = 3
+    }
+}
