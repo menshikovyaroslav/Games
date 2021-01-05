ac23b9fadaaf01fbe94eaa81fa8f4d307527c54f
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2021-01-05 22:16:28 +0700

    Проверки при загрузке словарей

--- EnglishTrainer/Classes/Dictionaries/Vocabulary.cs
+++ EnglishTrainer/Classes/Dictionaries/Vocabulary.cs
@@ -30,21 +30,33 @@ namespace EnglishTrainer.Classes.Dictionaries
             var beginnerLines = File.ReadAllLines("beginner.csv", Encoding.GetEncoding("windows-1251"));
             foreach (var line in beginnerLines)
             {
+                if (string.IsNullOrEmpty(line)) continue;
+
                 var splitted = line.Split(';');
+                if (splitted.Length != 2) continue;
+
                 _beginnerDictionary.Add(new Word() {Answer = splitted[0], Help = splitted[1] });
             }
 
             var standardLines = File.ReadAllLines("standard.csv", Encoding.GetEncoding("windows-1251"));
             foreach (var line in standardLines)
             {
+                if (string.IsNullOrEmpty(line)) continue;
+
                 var splitted = line.Split(';');
+                if (splitted.Length != 2) continue;
+
                 _standardDictionary.Add(new Word() { Answer = splitted[0], Help = splitted[1] });
             }
 
             var advancedLines = File.ReadAllLines("advanced.csv", Encoding.GetEncoding("windows-1251"));
             foreach (var line in advancedLines)
             {
+                if (string.IsNullOrEmpty(line)) continue;
+
                 var splitted = line.Split(';');
+                if (splitted.Length != 2) continue;
+
                 _advancedDictionary.Add(new Word() { Answer = splitted[0], Help = splitted[1] });
             }
 
