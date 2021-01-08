305da2ac14d03d0c23f2d043d39cfba75c3d5daf
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2021-01-08 22:38:30 +0700

    Рефакторинг

--- EnglishTrainer/Classes/SpaceShip.cs
+++ EnglishTrainer/Classes/SpaceShip.cs
@@ -1,10 +1,5 @@
-﻿using EnglishTrainer.Classes.Dictionaries;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace EnglishTrainer.Classes
--- EnglishTrainer/Classes/Word.cs
+++ EnglishTrainer/Classes/Word.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EnglishTrainer.Classes.Dictionaries
+namespace EnglishTrainer.Classes
 {
     public class Word : INotifyPropertyChanged
     {
