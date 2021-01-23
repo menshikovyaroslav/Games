14ff98b5b2327c6dbeab3e0884885faf55ccedf9
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2021-01-24 02:04:38 +0700

    Пингвин в отдельный контрол

--- TuxWantsFly/ViewModel/MainViewModel.cs
+++ TuxWantsFly/ViewModel/MainViewModel.cs
@@ -5,18 +5,6 @@ using TuxWantsFly.Commands;
 
 namespace TuxWantsFly.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
         #region Commands
@@ -25,7 +13,7 @@ namespace TuxWantsFly.ViewModel
 
         public void StartGameCommand_Execute()
         {
-            MessageBox.Show("ok");
+            
         }
 
         public bool StartGameCommand_CanExecute()
