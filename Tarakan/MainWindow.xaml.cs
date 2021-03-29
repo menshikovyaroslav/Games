fae9480edf9cb62674b9c14d41bbffeb256dc52e
menshikovyaroslav <menshikov.yaroslav@gmail.com>
2021-03-30 00:53:47 +0700

    Ок, проект Таракан в деле

---
+++ Tarakan/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tarakan
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int count = 0;
+        int maxCount = 10;
+
+        int tarakanMaxCount = 1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            while (count < maxCount)
+            {
+                if (count == 0)
+                {
+                    // создать таракана
+                }
+
+
+
+
+
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
